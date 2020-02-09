4d2d33407cdf4493810a6f51f86c2f938314a77a
Djisin <medija_teka@hotmail.com>
2020-02-09 19:41:26 +0100

    fixed redirecting issue

--- js/confirmation.js
+++ js/confirmation.js
@@ -5,5 +5,5 @@ localStorage.removeItem('orderId')
 orderP.innerText = orderId;
 localStorage.clear();
 setTimeout(function(){
-    window.location.href = '/list-view.html';
+    window.location.href = 'list-view.html';
  }, 10000);
